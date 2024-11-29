78f2cde12a388c038b36234371a729835bb85d8b
Max Rimlinger <37249275+maxrimlinger@users.noreply.github.com>
2024-11-29 10:35:55 -0500

    Begin factoring out movement into separate functions - no func. yet

--- main.c
+++ main.c
@@ -15,6 +15,63 @@ const size_t BUF_INCR = 64;
 const int NORMAL_KEYS_LEN = 96;
 const char *NORMAL_KEYS = "`~1!2@3#4$5%6^7&8*9(0)-_=+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
 
+typedef struct CurPos {
+    int x;
+    int y;
+};
+
+CurPos move_up(int *buf_idx) {
+
+}
+
+CurPos move_down(int *buf_idx) {
+
+}
+
+CurPos move_left(int *buf_idx) {
+
+}
+
+CurPos move_right(int *buf_idx) {
+
+}
+
+void loop(char *buffer) {
+    int cur_x = 0;
+    int cur_y = 0;
+    int buf_idx = 0;
+    printw(buffer);
+    refresh();
+    while (1) {
+        move(cur_x, cur_y);
+        int key = getch();
+        switch (key) {
+            case KEY_UP:
+                CurPos pos = move_up(&buf_idx);
+                move(pos.x, pos.y);
+                break;
+            case KEY_DOWN:
+                CurPos pos = move_down(&buf_idx);
+                move(pos.x, pos.y);
+                break;
+            case KEY_LEFT:
+                CurPos pos = move_left(&buf_idx);
+                move(pos.x, pos.y);
+                break;
+            case KEY_RIGHT:
+                CurPos pos = move_right(&buf_idx);
+                move(pos.x, pos.y);
+                break;
+        }
+        // text insertion
+//        for (int i = 0; i < NORMAL_KEYS_SIZE; i++) {
+//            if (key == NORMAL_KEYS[i]) {
+//
+//            }
+//        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: mim <filename>\n");
@@ -43,33 +100,8 @@ int main(int argc, char *argv[]) {
     initscr();
     keypad(stdscr, TRUE);
 
-    int cur_x = 0;
-    int cur_y = 0;
-    printw(buffer);
-    refresh();
-    while (1) {
-        move(cur_x, cur_y);
-        int key = getch();
-        switch (key) {
-            case KEY_RIGHT:
-                cur_y++;
-                break;
-            case KEY_LEFT:
-                cur_y--;
-                break;
-            case KEY_UP:
-                cur_x--;
-                break;
-            case KEY_DOWN:
-                cur_x++;
-                break;
-        }
-//        for (int i = 0; i < NORMAL_KEYS_SIZE; i++) {
-//            if (key == NORMAL_KEYS[i]) {
-//
-//            }
-//        }
-    }
+    // main program loop
+    loop(buffer);
 
     free(buffer);
     endwin();
