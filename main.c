dcb0b80a901e6f8c7e3742262ce17443efdbcdef
Willow Rimlinger <git@willowr.dev>
2024-12-19 00:36:16 -0500

    Add different modes

--- main.c
+++ main.c
@@ -19,59 +19,92 @@
 static const int NORMAL_KEYS_LEN = 96;
 static const char *NORMAL_KEYS = "`~1!2@3#4$5%6^7&8*9(0)-_=+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>? ";
 
-void print_num(int y, int x, int num) { //FIXME remove
-        move(y, x);
-        clrtoeol();
-        char str[3];
-        sprintf(str, "%d", num);
-        printw(str);
-}
-
 static void loop(FileProxy fp, const char *filename) {
     View view = {0, 0, 0, 0, 0};
+    char mode = 'i'; // i - insert, n - normal
     while (1) {
         print_fp(fp, view);
         move_cur(view);
         refresh();
         int key = getch();
-        switch (key) {
-            case KEY_UP:
-                view = move_up(fp, view);
-                break;
-            case KEY_DOWN:
-                view = move_down(fp, view);
-                break;
-            case KEY_LEFT:
-                view = move_left(fp, view);
-                break;
-            case KEY_RIGHT:
-                view = move_right(fp, view);
-                break;
-            case KEY_END:
-                view = move_to_eol(fp, view);
-                break;
-            case KEY_HOME:
-                view = move_to_bol(fp, view);
-                break;
-            case KEY_ENTER:
-            case '\n':
-            case '\r':
-                view = insert_newline(&fp, view);
-                break;
-            case KEY_BACKSPACE:
-                view = backspace(&fp, view);
-                break;
-            case KEY_DC:
-                view = delete(&fp, view);
-                break;
-            case KEY_NPAGE: // FIXME make it something normal
-                write_fp(fp, filename);
-                break;
-        }
-        // text insertion
-        for (int i = 0; i < NORMAL_KEYS_LEN; i++) {
-            if (key == NORMAL_KEYS[i]) {
-                view = insert_char(key, &fp, view);
+        if (mode == 'i') { // insert mode
+            switch (key) {
+                case KEY_UP:
+                    view = move_up(fp, view);
+                    break;
+                case KEY_DOWN:
+                    view = move_down(fp, view);
+                    break;
+                case KEY_LEFT:
+                    view = move_left(fp, view);
+                    break;
+                case KEY_RIGHT:
+                    view = move_right(fp, view);
+                    break;
+                case KEY_END:
+                    view = move_to_eol(fp, view);
+                    break;
+                case KEY_HOME:
+                    view = move_to_bol(fp, view);
+                    break;
+                case KEY_ENTER:
+                case '\n':
+                case '\r':
+                    view = insert_newline(&fp, view);
+                    break;
+                case KEY_BACKSPACE:
+                    view = backspace(&fp, view);
+                    break;
+                case KEY_DC:
+                    view = delete(&fp, view);
+                    break;
+                case 27:
+                    mode = 'n';
+                    break;
+            }
+            // text insertion
+            for (int i = 0; i < NORMAL_KEYS_LEN; i++) {
+                if (key == NORMAL_KEYS[i]) {
+                    view = insert_char(key, &fp, view);
+                }
+            }
+        } else { // normal mode
+            switch (key) {
+                case KEY_UP:
+                    view = move_up(fp, view);
+                    break;
+                case KEY_DOWN:
+                    view = move_down(fp, view);
+                    break;
+                case KEY_LEFT:
+                    view = move_left(fp, view);
+                    break;
+                case KEY_RIGHT:
+                    view = move_right(fp, view);
+                    break;
+                case KEY_END:
+                case '$':
+                    view = move_to_eol(fp, view);
+                    break;
+                case KEY_HOME:
+                case '0':
+                    view = move_to_bol(fp, view);
+                    break;
+                case KEY_ENTER:
+                case '\n':
+                case '\r':
+                    // TODO move down, move to first non whitespace char
+                    break;
+                case KEY_BACKSPACE:
+                    // TODO move left, or move up a line if on first char
+                    break;
+                case KEY_DC:
+                case 'x':
+                    view = delete(&fp, view);
+                    break;
+                case 'i':
+                    mode = 'i';
+                    break;
             }
         }
     }
