291a8edaf178df6da3cecd77f083a2f5acd73840
Willow Rimlinger <git@willowr.dev>
2024-12-30 01:40:59 -0800

    Add b command

--- text_objects.h
+++ text_objects.h
@@ -37,3 +37,14 @@ CurPos get_end_pos_tobj(FileProxy fp, CurPos current_pos, TextObject tobj);
  * @return the position in the fileproxy of the first character of the next text object
  */
 CurPos get_beg_pos_n_tobj(FileProxy fp, CurPos current_pos, TextObject tobj);
+
+/**
+ * Get the position in a FileProxy of the first character of the previous text object
+ * that the current_pos is not already inside.
+ *
+ * @param fp the fileproxy to find the text object in
+ * @param current_pos the current cursor position
+ * @param tobj the text object to find the first character of
+ * @return the position in the fileproxy of the first character of the previous text object
+ */
+CurPos get_beg_pos_p_tobj(FileProxy fp, CurPos current_pos, TextObject tobj);
